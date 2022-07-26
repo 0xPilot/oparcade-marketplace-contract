550880f456a7d575b9744ccce0c1d46c2989b91f
0xPilot <pilot.majors@gmail.com>
2022-07-26 23:06:26 +0900

    add IAddressRegistry interface

---
+++ contracts/interfaces/IAddressRegistry.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+interface IAddressRegistry {
+  function tokenRegistry() external view returns (address);
+
+  function marketplace() external view returns (address);
+}
