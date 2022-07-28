643eef1e49a67f5d82e9c6886ec26266db80f124
0xPilot <pilot.majors@gmail.com>
2022-07-28 21:33:56 +0900

    add collection check function on ITokeknRegistry interface

--- contracts/interfaces/ITokenRegistry.sol
+++ contracts/interfaces/ITokenRegistry.sol
@@ -2,5 +2,7 @@
 pragma solidity ^0.8.11;
 
 interface ITokenRegistry {
-  function enabled(address _token) external view returns (bool);
+  function enabledCollection(address _nftAddress) external view returns (bool);
+
+  function enabledPayToken(address _token) external view returns (bool);
 }
