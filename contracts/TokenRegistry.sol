f1d0312ac75e83da4f3e711590583eff266f0e5e
0xPilot <pilot.majors@gmail.com>
2022-07-28 21:33:22 +0900

    add collection add/remove functions and events

--- contracts/TokenRegistry.sol
+++ contracts/TokenRegistry.sol
@@ -5,14 +5,45 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract TokenRegistry is Initializable, OwnableUpgradeable {
-  event TokenAdded(address indexed by, address indexed token);
-  event TokenRemoved(address indexed by, address indexed token);
+  event CollectionAdded(address indexed by, address indexed collection);
+  event CollectionRemoved(address indexed by, address indexed collection);
+  event PayTokenAdded(address indexed by, address indexed token);
+  event PayTokenRemoved(address indexed by, address indexed token);
+
+  /// @notice NFTAddress -> Bool
+  mapping(address => bool) public enabledCollection;
 
   /// @notice Token -> Bool
-  mapping(address => bool) public enabled;
+  mapping(address => bool) public enabledPayToken;
 
   function initialize() external initializer {
     __Ownable_init();
+  }
+
+  /**
+   @notice Add NFT collection
+   @dev Only owner
+   @param _nftAddress NFT address
+   */
+  function addCollection(address _nftAddress) external onlyOwner {
+    require(!enabledCollection[_nftAddress], "collection already added");
+
+    enabledCollection[_nftAddress] = true;
+
+    emit CollectionAdded(msg.sender, _nftAddress);
+  }
+
+  /**
+   @notice Remove NFT collection
+   @dev Only owner
+   @param _nftAddress NFT address
+   */
+  function removeCollection(address _nftAddress) external onlyOwner {
+    require(enabledCollection[_nftAddress], "collection not exist");
+
+    enabledCollection[_nftAddress] = false;
+
+    emit CollectionRemoved(msg.sender, _nftAddress);
   }
 
   /**
@@ -20,12 +51,12 @@ contract TokenRegistry is Initializable, OwnableUpgradeable {
    @dev Only owner
    @param _token ERC20 token address
    */
-  function add(address _token) external onlyOwner {
-    require(!enabled[_token], "token already added");
+  function addPayToken(address _token) external onlyOwner {
+    require(!enabledPayToken[_token], "token already added");
 
-    enabled[_token] = true;
+    enabledPayToken[_token] = true;
 
-    emit TokenAdded(msg.sender, _token);
+    emit PayTokenAdded(msg.sender, _token);
   }
 
   /**
@@ -33,11 +64,11 @@ contract TokenRegistry is Initializable, OwnableUpgradeable {
    @dev Only owner
    @param _token ERC20 token address
    */
-  function remove(address _token) external onlyOwner {
-    require(enabled[_token], "token not exist");
+  function removePayToken(address _token) external onlyOwner {
+    require(enabledPayToken[_token], "token not exist");
 
-    enabled[_token] = false;
+    enabledPayToken[_token] = false;
 
-    emit TokenRemoved(msg.sender, _token);
+    emit PayTokenRemoved(msg.sender, _token);
   }
 }
