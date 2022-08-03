16bf83a4ac3c0e9b872e684b8987e646cf3c6bf7
0xPilot <pilot.majors@gmail.com>
2022-08-03 23:06:41 +0900

    add bid function on Auction contract

--- contracts/OPGamesAuction.sol
+++ contracts/OPGamesAuction.sol
@@ -5,14 +5,21 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
 import "./interfaces/IAddressRegistry.sol";
 import "./interfaces/ITokenRegistry.sol";
 
 // TODO: Add Pausable
 contract OPGamesAuction is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable, PausableUpgradeable {
+  using SafeERC20Upgradeable for IERC20Upgradeable;
+
   event AuctionCreated(address indexed nftAddress, uint256 indexed tokenId, address payToken);
+  event BidPlaced(address indexed nftAddress, uint256 indexed tokenId, address indexed bidder, uint256 bid);
+  event BidWithdrawn(address indexed nftAddress, uint256 indexed tokenId, address indexed bidder, uint256 bid);
+  event BidRefunded(address indexed nftAddress, uint256 indexed tokenId, address indexed bidder, uint256 bid);
 
   /// @notice Parameters of an auction
   struct Auction {
@@ -37,6 +44,9 @@ contract OPGamesAuction is Initializable, OwnableUpgradeable, ReentrancyGuardUpg
 
   /// @notice ERC721 Address -> Token ID -> highest bidder info (if a bid has been received)
   mapping(address => mapping(uint256 => HighestBid)) public highestBids;
+
+  /// @notice globally and across all auctions, the amount by which a bid has to increase
+  uint256 public minBidIncrement = 1;
 
   /// @notice Platform fee recipient
   address payable public feeRecipient;
@@ -91,6 +101,86 @@ contract OPGamesAuction is Initializable, OwnableUpgradeable, ReentrancyGuardUpg
     return block.timestamp;
   }
 
+  function placeBid(
+    address _nftAddress,
+    uint256 _tokenId,
+    uint256 _bidAmount
+  ) external payable nonReentrant whenNotPaused {
+    require(msg.sender == tx.origin, "no contracts permitted");
+
+    // Check the auction to see if this is a valid bid
+    Auction memory auction = auctions[_nftAddress][_tokenId];
+
+    // Ensure auction is in flight
+    require(_getNow() >= auction.startTime && _getNow() <= auction.endTime, "bidding outside of the auction window");
+    // require(auction.payToken != address(0), "ERC20 method used for auction");
+
+    _placeBid(_nftAddress, _tokenId, _bidAmount);
+  }
+
+  function _placeBid(
+    address _nftAddress,
+    uint256 _tokenId,
+    uint256 _bidAmount
+  ) internal whenNotPaused {
+    Auction storage auction = auctions[_nftAddress][_tokenId];
+
+    if (auction.minBid == auction.reservePrice) {
+      require(_bidAmount >= auction.reservePrice, "bid cannot be lower than reserve price");
+    }
+
+    // Ensure bid adheres to outbid increment and threshold
+    HighestBid storage highestBid = highestBids[_nftAddress][_tokenId];
+    uint256 minBidRequired = highestBid.bid + minBidIncrement;
+
+    if (auction.payToken != address(0)) {
+      require(_bidAmount >= minBidRequired, "failed to outbid highest bidder");
+
+      IERC20Upgradeable payToken = IERC20Upgradeable(auction.payToken);
+      payToken.safeTransferFrom(msg.sender, address(this), _bidAmount);
+    } else {
+      require(msg.value >= minBidRequired, "failed to outbid highest bidder");
+    }
+
+    // Refund existing top bidder if found
+    if (highestBid.bidder != address(0)) {
+      _refundHighestBidder(_nftAddress, _tokenId, highestBid.bidder, highestBid.bid);
+    }
+
+    // assign top bidder and bid time
+    highestBid.bidder = payable(msg.sender);
+    highestBid.bid = _bidAmount;
+    highestBid.lastBidTime = _getNow();
+
+    emit BidPlaced(_nftAddress, _tokenId, msg.sender, _bidAmount);
+  }
+
+  function withdrawBid(address _nftAddress, uint256 _tokenId) external nonReentrant whenNotPaused {
+    HighestBid storage highestBid = highestBids[_nftAddress][_tokenId];
+
+    // Ensure highest bidder is the caller
+    require(highestBid.bidder == msg.sender, "you are not the highest bidder");
+
+    uint256 _endTime = auctions[_nftAddress][_tokenId].endTime;
+
+    require(
+      _getNow() > _endTime && (_getNow() - _endTime >= 43200),
+      "can withdraw only after 12 hours (after auction ended)"
+    );
+
+    uint256 previousBid = highestBid.bid;
+
+    // Clean up the existing top bid
+    delete highestBids[_nftAddress][_tokenId];
+
+    // Refund the top bidder
+    _refundHighestBidder(_nftAddress, _tokenId, payable(msg.sender), previousBid);
+
+    emit BidWithdrawn(_nftAddress, _tokenId, msg.sender, previousBid);
+  }
+
+  function resultAuction(address _nftAddress, uint256 _tokenId) external nonReentrant {}
+
   function _createAuction(
     address _nftAddress,
     uint256 _tokenId,
@@ -126,5 +216,24 @@ contract OPGamesAuction is Initializable, OwnableUpgradeable, ReentrancyGuardUpg
     });
 
     emit AuctionCreated(_nftAddress, _tokenId, _payToken);
+  }
+
+  function _refundHighestBidder(
+    address _nftAddress,
+    uint256 _tokenId,
+    address payable _currentHighestBidder,
+    uint256 _currentHighestBid
+  ) private {
+    Auction memory auction = auctions[_nftAddress][_tokenId];
+    if (auction.payToken == address(0)) {
+      // refund previous best (if bid exists)
+      (bool successRefund, ) = _currentHighestBidder.call{value: _currentHighestBid}("");
+      require(successRefund, "failed to refund previous bidder");
+    } else {
+      IERC20Upgradeable payToken = IERC20Upgradeable(auction.payToken);
+      payToken.safeTransfer(_currentHighestBidder, _currentHighestBid);
+    }
+
+    emit BidRefunded(_nftAddress, _tokenId, _currentHighestBidder, _currentHighestBid);
   }
 }
