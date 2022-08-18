bb420c00771228e393f602141863c7df4c7ae2e2
0xPilot <pilot.majors@gmail.com>
2022-08-18 22:39:54 +0900

    add ERC721Factory contract

---
+++ contracts/ERC721Factory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "./ERC721Mintable.sol";
+import "./interfaces/IERC721Mintable.sol";
+
+contract ERC721Factory {
+  constructor() {}
+
+  function createERC721(
+    string memory _name,
+    string memory _symbol,
+    string memory _baseTokenURI,
+    address _to,
+    uint256 _tokenCount
+  ) external returns (address) {
+    address collection = address(new ERC721Mintable(msg.sender, _name, _symbol, _baseTokenURI, _to, _tokenCount));
+    
+    return collection;
+  }
+}
