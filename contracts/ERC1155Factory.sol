d1b98dce0f7a6b0f225bc2ff6908c037b915200d
0xPilot <pilot.majors@gmail.com>
2022-08-25 12:31:38 +0900

    add event and soldocs, and rename function

--- contracts/ERC1155Factory.sol
+++ contracts/ERC1155Factory.sol
@@ -5,17 +5,33 @@ import "./ERC1155Mintable.sol";
 import "./interfaces/IERC1155Mintable.sol";
 
 contract ERC1155Factory {
+  event ERC1155CollectionMinted(address indexed by, string name, string symbol, address to);
+
   constructor() {}
 
-  function createERC1155(
+  /**
+   * @notice Mint ERC1155 collection
+   * @param _name Collection name
+   * @param _symbol Collection symbol
+   * @param _to Collection receiver
+   * @param _ids Token Id array to mint
+   * @param _amounts Token amount array to mint
+   * @param _data Data
+   */
+  function mintERC1155Collection(
     string memory _name,
     string memory _symbol,
     string memory _baseTokenURI,
     address _to,
+    uint256[] memory _ids,
     uint256[] memory _amounts,
     bytes memory _data
   ) external returns (address) {
-    address collection = address(new ERC1155Mintable(msg.sender, _name, _symbol, _baseTokenURI, _to, _amounts, _data));
+    address collection = address(
+      new ERC1155Mintable(msg.sender, _name, _symbol, _baseTokenURI, _to, _ids, _amounts, _data)
+    );
+
+    emit ERC1155CollectionMinted(msg.sender, _name, _symbol, _to);
 
     return collection;
   }
