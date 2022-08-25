fe3291983f59c409f9e21ef8ab2211c15c95510f
0xPilot <pilot.majors@gmail.com>
2022-08-25 12:18:25 +0900

    add event and soldocs

--- contracts/ERC721Factory.sol
+++ contracts/ERC721Factory.sol
@@ -5,8 +5,18 @@ import "./ERC721Mintable.sol";
 import "./interfaces/IERC721Mintable.sol";
 
 contract ERC721Factory {
+  event ERC721CollectionMinted(address indexed by, string name, string symbol, address to);
+
   constructor() {}
 
+  /**
+   * @notice Mint ERC721 collection
+   * @param _name Collection name
+   * @param _symbol Collection symbol
+   * @param _baseTokenURI Colleciton base token URI
+   * @param _to Collection receiver
+   * @param _tokenCount NFT count to mint
+   */
   function createERC721(
     string memory _name,
     string memory _symbol,
@@ -15,6 +25,8 @@ contract ERC721Factory {
     uint256 _tokenCount
   ) external returns (address) {
     address collection = address(new ERC721Mintable(msg.sender, _name, _symbol, _baseTokenURI, _to, _tokenCount));
+
+    emit ERC721CollectionMinted(msg.sender, _name, _symbol, _to);
 
     return collection;
   }
