0b2fb45202fe3623004841daeea764af93dba907
0xPilot <pilot.majors@gmail.com>
2022-08-25 12:18:48 +0900

    add soldocs on ERC721Mintable contract

--- contracts/ERC721Mintable.sol
+++ contracts/ERC721Mintable.sol
@@ -4,8 +4,13 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract ERC721Mintable is ERC721Enumerable {
+  /// @dev Collection creator
   address public creator;
+
+  /// @dev Current Token Index
   uint256 public currentTokenId;
+
+  /// @dev Base token URI
   string private baseTokenURI;
 
   constructor(
@@ -22,16 +27,31 @@ contract ERC721Mintable is ERC721Enumerable {
     _mintTokens(_to, _tokenCount);
   }
 
+  /**
+   * @dev Returns the base token URI
+   */
   function _baseURI() internal view virtual override returns (string memory) {
     return baseTokenURI;
   }
 
+  /**
+   * @notice Mint ERC721 NFTs
+   * @dev Only collection creator
+   * @param _to NFT receiver
+   * @param _tokenCount NFT count to mint
+   */
   function mint(address _to, uint256 _tokenCount) external {
     require(msg.sender == creator, "only creator");
 
     _mintTokens(_to, _tokenCount);
   }
 
+  /**
+   * @dev Mint initial ERC721 NFTs
+   * @dev Only called by the ERC721Factory contract when creating a new collection
+   * @param _to Collection receiver
+   * @param _tokenCount NFT count to mint
+   */
   function _mintTokens(address _to, uint256 _tokenCount) private {
     for (uint256 i; i < _tokenCount; i++) {
       currentTokenId++;
