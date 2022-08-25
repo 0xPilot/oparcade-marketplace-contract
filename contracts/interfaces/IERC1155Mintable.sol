8a0a24d9e0455c06bed334362de3ce643f951836
0xPilot <pilot.majors@gmail.com>
2022-08-25 12:33:30 +0900

    update IERC1155Mintable interface

--- contracts/interfaces/IERC1155Mintable.sol
+++ contracts/interfaces/IERC1155Mintable.sol
@@ -4,12 +4,7 @@ pragma solidity ^0.8.11;
 interface IERC1155Mintable {
   function mint(
     address _to,
-    uint256 _amount,
-    bytes memory _data
-  ) external;
-
-  function mintBatch(
-    address _to,
+    uint256[] memory _ids,
     uint256[] memory _amounts,
     bytes memory _data
   ) external;
