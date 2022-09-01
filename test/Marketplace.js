4a7636ab8fb4a057567d1fa31eacfd4ebd62b074
0xPilot <pilot.majors@gmail.com>
2022-09-01 22:33:49 +0900

    add unit tests for listing feature on Marketplace contract

--- test/Marketplace.js
+++ test/Marketplace.js
@@ -37,9 +37,17 @@ describe("Marketplace", () => {
     const Marketplace = await ethers.getContractFactory("OPGamesMarketplace");
     marketplace = await upgrades.deployProxy(Marketplace, [addressRegistry.address, feeRecipient.address, platformFee]);
 
+    // deploy mock ERC20 token
+    const ERC20Mock = await ethers.getContractFactory("ERC20Mock");
+    mockERC20 = await ERC20Mock.deploy("mockToken", "mockToken");
+
     // deploy mock ERC721 token
     const MockERC721 = await ethers.getContractFactory("MockERC721");
     mockERC721 = await MockERC721.deploy();
+
+    // Register the contract addresses
+    await addressRegistry.updateTokenRegistry(tokenRegistry.address);
+    await addressRegistry.updateMarketplace(marketplace.address);
 
     // Mint NFTs
     await mockERC721.mint(minter.address, 1);
@@ -47,7 +55,7 @@ describe("Marketplace", () => {
   });
 
   describe("listItem", () => {
-    it("Should revert when not owning NFT", async () => {
+    it("Should revert if not owning NFT", async () => {
       await expect(
         marketplace.listItem(
           mockERC721.address,
@@ -58,6 +66,64 @@ describe("Marketplace", () => {
           await getCurrentBlockTimestamp(),
         ),
       ).to.be.revertedWith("not owning item");
+    });
+
+    it("Should revert if not approved", async () => {
+      await expect(
+        marketplace
+          .connect(minter)
+          .listItem(mockERC721.address, firstTokenId, 1, ZERO_ADDRESS, pricePerItem, await getCurrentBlockTimestamp()),
+      ).to.be.revertedWith("item not approved");
+    });
+
+    it("Should revert if the collection is not approved", async () => {
+      await mockERC721.connect(minter).setApprovalForAll(marketplace.address, true);
+      await expect(
+        marketplace
+          .connect(minter)
+          .listItem(mockERC721.address, firstTokenId, 1, ZERO_ADDRESS, pricePerItem, await getCurrentBlockTimestamp()),
+      ).to.be.revertedWith("invalid collection");
+    });
+
+    it("Should revert if the payment token is not approved", async () => {
+      await tokenRegistry.addCollection(mockERC721.address);
+      await mockERC721.connect(minter).setApprovalForAll(marketplace.address, true);
+      await expect(
+        marketplace
+          .connect(minter)
+          .listItem(
+            mockERC721.address,
+            firstTokenId,
+            1,
+            mockERC20.address,
+            pricePerItem,
+            await getCurrentBlockTimestamp(),
+          ),
+      ).to.be.revertedWith("invalid pay token");
+    });
+
+    it("Should list the item with the native token", async () => {
+      await tokenRegistry.addCollection(mockERC721.address);
+      await mockERC721.connect(minter).setApprovalForAll(marketplace.address, true);
+      await marketplace
+        .connect(minter)
+        .listItem(mockERC721.address, firstTokenId, 1, ZERO_ADDRESS, pricePerItem, await getCurrentBlockTimestamp());
+    });
+
+    it("Should list the item with the ERC20 token", async () => {
+      await tokenRegistry.addCollection(mockERC721.address);
+      await tokenRegistry.addPayToken(mockERC20.address);
+      await mockERC721.connect(minter).setApprovalForAll(marketplace.address, true);
+      await marketplace
+        .connect(minter)
+        .listItem(
+          mockERC721.address,
+          firstTokenId,
+          1,
+          mockERC20.address,
+          pricePerItem,
+          await getCurrentBlockTimestamp(),
+        );
     });
   });
 });
